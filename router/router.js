dcf46f0b4db6815f38a7b53d31142922501253f3
Amanilogsolution <aman@ilogsolution.com>
2022-05-26 15:59:23 +0530

    push

---
+++ router/router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const GeneratorController = require('../controller/GeneratorMaster')
+const DieselController = require('../controller/DieselMaster')
+const VisiterController = require('../controller/VisiterMaster')
+const LoginController = require('../controller/LoginMaster')
+const WarehouseController = require('../controller/WarehouseMaster')
+
+
+router.post('/generatorentry',GeneratorController.GeneratorEntry)
+router.post('/dieselentry',DieselController.DieselEntry)
+router.post('/visiterentry',VisiterController.VisiterEntry)
+router.post('/loginuser',LoginController.UserLogin)
+router.post('/warehousecheckopen',WarehouseController.Warehousecheckopen)
+router.post('/warehouseopen',WarehouseController.Warehouseopen)
+router.post('/warehouseclose',WarehouseController.Warehouseclose)
+router.get('/warehouseLastclose',WarehouseController.WarehouseLastclose)
+
+
+router.post('/allemployee',VisiterController.Allemployee)
+
+module.exports= router
