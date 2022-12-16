c239dae22fc56d3841a67fe628197a39486f71cc
Amanilogsolution <aman@ilogsolution.com>
2022-12-16 12:21:41 +0530

    push

--- controller/Guards/GuardHistory.js
+++ controller/Guards/GuardHistory.js
@@ -9,7 +9,9 @@ async function TotalGuardsHistory (req,res){
     try{
         const pool = new sql.ConnectionPool(sqlConfig);
         await pool.connect();
-        const result = await pool.query(`select *,convert(varchar(15),logindate,105)  as LoginDate,convert(varchar,LoginTime,8)  as timeLogin,convert(varchar(15),Logoutdate,105)  as LogOutDate,convert(varchar,Logouttime,8)  as timeLoginout  from NEWAWLDB.dbo.tbl_guardlogs where  convert(date,logindate) between '${startDate}' and '${endDate}' `)
+        const result = await pool.query(`select tbl_guardlogs.*,tbl_guardmaster.Shift ,convert(varchar(15),logindate,105)  as LoginDate,convert(varchar,LoginTime,8)  as timeLogin,convert(varchar(15),Logoutdate,105)  as LogOutDate,convert(varchar,Logouttime,8)  as timeLoginout  from NEWAWLDB.dbo.tbl_guardlogs
+        join tbl_guardmaster on tbl_guardmaster.Guardid = tbl_guardlogs.Guardid 
+        where  convert(date,logindate) between  '${startDate}' and '${endDate}'`)
         await pool.close() 
         res.send(result.recordset)
     }
